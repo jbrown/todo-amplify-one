802dafb07c4b69a5453a20e332aa4219eb80ef93
Justin Brown <justinbrown.work@gmail.com>
2020-01-10 08:06:06 -0600

    Add basic navbar with ability to logout.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
-import Amplify from "aws-amplify";
+import Amplify, { Auth } from "aws-amplify";
 import awsconfig from "./aws-exports";
 import { withAuthenticator } from "aws-amplify-react";
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+
+import Navbar from "./components/Navbar";
 
 Amplify.configure(awsconfig);
 
 function App() {
-  return <div className="container">App</div>;
+  return (
+    <div className="container">
+      <Navbar onSignOut={() => Auth.signOut()} />
+    </div>
+  );
 }
 
-export default withAuthenticator(App, true);
+export default withAuthenticator(App, false);
