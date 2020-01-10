447986dc2fb9f4692e1303c5c6807f93e3573465
Justin Brown <justinbrown.work@gmail.com>
2020-01-10 07:31:43 -0600

    Add Bootstrap css.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import Amplify from "aws-amplify";
 import awsconfig from "./aws-exports";
 import { withAuthenticator } from "aws-amplify-react";
+import "bootstrap/dist/css/bootstrap.css";
 
 Amplify.configure(awsconfig);
 
 function App() {
-  return <div className="App">App</div>;
+  return <div className="container">App</div>;
 }
 
 export default withAuthenticator(App, true);
